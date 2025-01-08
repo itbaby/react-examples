8d6f34c10d9949f072768420b811f646d1a19da1
leewin <jquery@hotmail.com>
2025-01-08 21:06:35 +0800

    try to fix any
    how to avoid any, we can add interfact for props

--- src/useEffectExample.jsx
+++ src/useEffectExample.jsx
@@ -5,27 +5,25 @@ for most side effects
 runs after paint
 asynchronous
 used more often
-
-**/
-export default function UseEffectExample(){
+*/
+export default function UseEffectExample() {
   const ref = useRef(null)
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0)
 
-  useLayoutEffect(()=>{
-    if(ref.current){
-      
-    const {height} = ref.current.getBoundingClientRect()
-    setCount(height)
+  useLayoutEffect(() => {
+    if (ref.current) {
+      const { height } = ref.current.getBoundingClientRect()
+      setCount(height)
     }
-  },[])
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     document.title = `You clicked ${count} times`
-  },[count])
+  }, [count])
 
   return (
     <>
-      <button ref={ref} onClick={()=>setCount(count+1)}>Change Page Title</button>
+      <button ref={ref} onClick={() => setCount(count + 1)}>Change Page Title</button>
     </>
   )
 }
